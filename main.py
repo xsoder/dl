a0755a51afac721074e3482b71ed21f163ba3cbc
xsoder <tadihailukebe@gmail.com>
2026-01-30 16:06:22 -0500

    Crazy level of refactoring and asm generation.

--- main.py
+++ main.py
@@ -1,142 +1,122 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, subprocess, os
 
 SYM_TOK = ['+', '-', '*', '/', '(', ')', '>', '<', '=', ':', ';', '{', '}']
 TYPE_TOK = ['i32']
 KEY_TOK = ['main', 'return']
 
+class CompileError(Exception):
+    pass
+
 class Token:
-    def __init__(self, typ, val):
+    def __init__(self, typ, val, line):
         self.typ = typ
         self.val = val
-
-    def __repr__(self):
-        return f"Token(typ='{self.typ}', val='{self.val}')\n"
+        self.line = line
 
 def tokenize(input_string):
     toks = []
     current = ""
+    line = 1
+
+    def emit_current():
+        nonlocal current
+        if not current:
+            return
+        if current.isdigit():
+            toks.append(Token('NUM', int(current), line))
+        elif current in TYPE_TOK:
+            toks.append(Token('TYPE', current, line))
+        elif current in KEY_TOK:
+            toks.append(Token('KEY', current, line))
+        else:
+            toks.append(Token('VAR', current, line))
+        current = ""
+
     for ch in input_string:
-        if ch in SYM_TOK:
-            if current:
-                if current.isdigit():
-                    toks.append(Token('NUM', int(current)))
-                elif current in TYPE_TOK:
-                    toks.append(Token('TYPE', current))
-                elif current in KEY_TOK:
-                    toks.append(Token('KEY', current))
-                else:
-                    toks.append(Token('VAR', current))
-                current = ""
-            toks.append(Token('SYM', ch))
+        if ch == '\n':
+            emit_current()
+            line += 1
+        elif ch in SYM_TOK:
+            emit_current()
+            toks.append(Token('SYM', ch, line))
         elif ch.isspace():
-            if current:
-                if current.isdigit():
-                    toks.append(Token('NUM', int(current)))
-                elif current in TYPE_TOK:
-                    toks.append(Token('TYPE', current))
-                elif current in KEY_TOK:
-                    toks.append(Token('KEY', current))
-                else:
-                    toks.append(Token('VAR', current))
-                current = ""
+            emit_current()
         else:
             current += ch
 
-    if current:
-        if current.isdigit():
-            toks.append(Token('NUM', int(current)))
-        elif current in TYPE_TOK:
-            toks.append(Token('TYPE', current))
-        elif current in KEY_TOK:
-            toks.append(Token('KEY', current))
-        else:
-            toks.append(Token('VAR', current))
-
+    emit_current()
     return toks
 
-def extract_token(tokens, sym_type, sym_name):
-    return [token for token in tokens if token.typ == sym_type and token.val == sym_name]
+def read_file(path):
+    with open(path) as f:
+        return f.read()
 
-def read_file(file_path):
-    with open(file_path) as fs:
-        return fs.read()
+def extract_token(tokens, typ, val):
+    return [t for t in tokens if t.typ == typ and t.val == val]
 
-def find_entry(tok, file_path):
-    main_sym = 'main'
-    entry = extract_token(tok, 'KEY', main_sym)
-    if len(entry) == 0:
-        print(f"{file_path}:0: ERROR: Could not find entry point {main_sym}")
-        exit(1)
-    else:
-        print(f"Entry point found at {file_path}")
-
+def find_entry(tokens, file_path):
+    if not extract_token(tokens, 'KEY', 'main'):
+        raise CompileError(f"{file_path}: ERROR: missing entry point main")
 
 def parse_file(file_path):
     content = read_file(file_path)
-    tk = tokenize(content)
-    find_entry(tk, file_path)
-    p = Parser(tk)
-    ast = p.parse_function()
-    return ast
-
-def print_ast(node, indent=0):
-    prefix = ' ' * indent
-    if isinstance(node, Function):
-        print(f"{prefix}Function: {node.name.val}")
-        for stmt in node.body:
-            print_ast(stmt, indent + 2)
-    elif isinstance(node, Variable):
-        print(f"{prefix}Variable: {node.val}")
-    elif isinstance(node, Number):
-        print(f"{prefix}Number: {node.val}")
-    elif isinstance(node, VarDecl):
-        print(f"{prefix}VarDec: {node.val}: {node.typ}")
-        print_ast(node.expr, indent + 2)
-    elif isinstance(node, Return):
-        print(f"{prefix}Return:")
-        print_ast(node.expr, indent + 2)
-    else:
-        assert False, "Unreachable"
+    tokens = tokenize(content)
+    find_entry(tokens, file_path)
+    parser = Parser(tokens, file_path)
+    return parser.parse_function()
 
 class Parser:
-    def __init__(self, token):
-        self.token = token
+    def __init__(self, tokens, file_path):
+        self.tokens = tokens
         self.pos = 0
+        self.file_path = file_path
+        self.sym = {}
+
+    def error(self, msg, tok=None):
+        line = tok.line if tok else 0
+        raise CompileError(f"{self.file_path}:{line}: ERROR: {msg}")
 
     def peek(self):
-        if self.pos < len(self.token):
-            return self.token[self.pos]
+        if self.pos < len(self.tokens):
+            return self.tokens[self.pos]
         return None
 
-    def consume(self, typ = None, val = None):
+    def consume(self, typ=None, val=None):
         tok = self.peek()
         if tok is None:
-            print(f"Unexpected token type {tok.typ} and name {tok.val}")
-            return
-        if tok.typ != typ and typ:
-            print(f"Unexpected token type {tok.typ} and name {tok.val}")
-            return
-        elif tok.val != val and val:
-            print(f"Unexpected token type {tok.typ} and name {tok.val}")
-            return
+            self.error("unexpected end of file")
+        if typ and tok.typ != typ:
+            self.error(f"expected {typ}, got {tok.typ}", tok)
+        if val and tok.val != val:
+            self.error(f"expected '{val}', got '{tok.val}'", tok)
         self.pos += 1
         return tok
 
-    def get_current_token(self):
-        if self.pos < len(self.token):
-            return self.token[self.pos]
-        return None
+    def parse_function(self):
+        self.consume('KEY', 'main')
+        self.consume('SYM', ':')
+        self.consume('SYM', ':')
+        self.consume('TYPE', 'i32')
+        self.consume('SYM', '{')
+        body = self.parse_block()
+        self.consume('SYM', '}')
+        return Function(body)
+
+    def parse_block(self):
+        stmts = []
+        while self.peek().val != '}':
+            stmts.append(self.parse_statement())
+        return stmts
 
     def parse_statement(self):
-        tk = self.get_current_token()
-        if tk.typ == 'VAR':
+        tok = self.peek()
+        if tok.typ == 'VAR':
             return self.parse_decl()
-        elif tk.typ == 'KEY' and tk.val == 'return':
-            return self.parse_ret()
-        else:
-            assert False, "Not implemented"
+        if tok.typ == 'KEY' and tok.val == 'return':
+            return self.parse_return()
+        self.error("invalid statement", tok)
 
     def parse_decl(self):
         name = self.consume('VAR')
@@ -145,69 +125,61 @@ class Parser:
         self.consume('SYM', '=')
         expr = self.parse_expr()
         self.consume('SYM', ';')
+
+        if expr.typ is None:
+            expr.typ = typ.val
+
+        if expr.typ != typ.val:
+            self.error(f"type mismatch: cannot assign {expr.typ} to {typ.val}", name)
+
+        self.sym[name.val] = typ.val
         return VarDecl(name.val, typ.val, expr)
 
-    def parse_ret(self):
-        self.consume('KEY', 'return')
+    def parse_return(self):
+        tok = self.consume('KEY', 'return')
         expr = self.parse_expr()
         self.consume('SYM', ';')
+
+        if expr.typ is None:
+            expr.typ = 'i32'
+
+        if expr.typ != 'i32':
+            self.error(f"return type {expr.typ}, expected i32", tok)
+
         return Return(expr)
 
-    def parse_function(self):
-        name = self.consume('KEY', 'main')
-
-        self.consume('SYM', ':')
-        self.consume('SYM', ':')
-        self.consume('TYPE', 'i32')
-        self.consume('SYM', '{')
-
-        body = self.parse_block()
-
-        self.consume('SYM', '}')
-        return Function(name, None, body)
-
     def parse_expr(self):
-        tok = self.get_current_token()
+        tok = self.peek()
+
+        if tok.typ == 'NUM':
+            self.consume('NUM')
+            return IntLiteral(tok.val)
+
         if tok.typ == 'VAR':
             self.consume('VAR')
-            return Variable(tok.val)
-        elif tok.typ == 'NUM':
-            self.consume('NUM')
-            return Number(tok.val, tok.typ)
-        else:
-            assert False, "Unreachable"
+            if tok.val not in self.sym:
+                self.error(f"undeclared variable '{tok.val}'", tok)
+            return Variable(tok.val, self.sym[tok.val])
 
-    def parse_block(self):
-        statements = []
-        while self.get_current_token().val != '}':
-            statements.append(self.parse_statement())
-        return statements
+        self.error("invalid expression", tok)
 
 class Function:
-    def __init__(self, name, param, body):
-        self.name = name
-        self.param = param
+    def __init__(self, body):
         self.body = body
 
-class Binop:
-    def __init__(self, lhs, rhs, op):
-        self.lhs = lhs
-        self.rhs = rhs
-        self.op = op
-
-class Number:
-    def __init__(self, val, typ):
+class IntLiteral:
+    def __init__(self, val):
         self.val = val
-        self.typ = typ
+        self.typ = None
 
 class Variable:
-    def __init__(self, val, typ='i32'):
-        self.val = val
+    def __init__(self, name, typ):
+        self.val = name
         self.typ = typ
 
 class VarDecl:
-    def __init__(self, val, typ, expr):
-        self.val = val
+    def __init__(self, name, typ, expr):
+        self.name = name
         self.typ = typ
         self.expr = expr
 
@@ -215,11 +187,95 @@ class Return:
     def __init__(self, expr):
         self.expr = expr
 
+class CodeGen:
+    def __init__(self, filename='a.asm'):
+        self.out = []
+        self.filename = filename
+
+    def emit(self, line):
+        self.out.append(line)
+
+    def generate(self, node):
+        if isinstance(node, Function):
+            self.generate_func(node)
+        elif isinstance(node, VarDecl):
+            self.generate_vardec(node)
+        elif isinstance(node, Return):
+            self.generate_return(node)
+        elif isinstance(node, Variable):
+            self.generate_var(node)
+        elif isinstance(node, IntLiteral):
+            self.generate_intlit(node)
+        else:
+            assert False, "Unreachable"
+            
+    def generate_func(self, fn):
+        self.emit("format ELF64")
+        self.emit("public main")
+        self.emit("section '.text' executable")
+        self.emit("")
+        self.emit("main:")
+        self.emit("        push rbp")
+        self.emit("        mov rbp, rsp")
+
+        for stmt in fn.body:
+            self.generate(stmt)
+
+        self.emit("        mov eax, 0")
+        self.emit("        pop rbp")
+        self.emit("        ret\n")
+    def generate_vardec(self, vardecl):
+        self.emit("        ;; --TODO--Unimplemnted vardecl")
+    def generate_var(self, vardecl):
+        self.emit("        ;; --TODO--Unimplemnted var")
+    def generate_intlit(self, intlit):
+        self.emit("        ;; --TODO--Unimplemnted intliteral")
+    def generate_return(self, retval):
+        self.emit("        ;; --TODO--Unimplemnted return")
+
+    def write_file(self):
+        with open(self.filename, 'w') as f:
+            f.write('\n'.join(self.out))
+            return self.filename
+        
+def print_ast(node, indent=0):
+    p = ' ' * indent
+    if isinstance(node, Function):
+        print(f"{p}Function")
+        for s in node.body:
+            print_ast(s, indent + 2)
+    elif isinstance(node, VarDecl):
+        print(f"{p}VarDecl {node.name}:{node.typ}")
+        print_ast(node.expr, indent + 2)
+    elif isinstance(node, Return):
+        print(f"{p}Return")
+        print_ast(node.expr, indent + 2)
+    elif isinstance(node, Variable):
+        print(f"{p}Variable {node.val}")
+    elif isinstance(node, IntLiteral):
+        print(f"{p}IntLiteral {node.val}")
+
+def usage():
+    print("USAGE: dl <file>")
+    sys.exit(1)
+
+def compilation(inputfile, objfile='a.o', outputfile='a.out'):
+    if inputfile == None:
+        usage()
+        exit(1)
+    subprocess.run(['fasm', inputfile, objfile])
+    subprocess.run(['gcc', objfile, '-o', outputfile])
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("USAGE: dl ./filename")
-        exit(1)
+        usage()
+    try:
+        ast = parse_file(sys.argv[1])
+        cd = CodeGen()
+        cd.generate(ast)
+        inputfile = cd.write_file()
+        compilation(inputfile)
 
-    file_path = sys.argv[1]
-    ast = parse_file(file_path)
-    print_ast(ast)
+    except CompileError as e:
+        print(e)
+        sys.exit(1)
